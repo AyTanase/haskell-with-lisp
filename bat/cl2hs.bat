daaf8e44317545e7b73b01b2a1ed3c60327659fc
AyTanase <ay.tanase@gmail.com>
2019-01-08 08:40:26 +0900

    cl2hs: Add option "all"

--- bat/cl2hs.bat
+++ bat/cl2hs.bat
@@ -1,1 +1,26 @@
-@runcl "%~dp0\..\lisp\haskell.lisp" --eval "(hs:compile \"%~1\")"
+@echo off
+set SRC="%~dp0\..\lisp\haskell.lisp"
+if %~1==all (call :compile-all) else (call runcl %SRC% --eval "(hs:compile \"%~1\")")
+exit /b
+
+:compile-all
+setlocal
+set "FILES="
+call :collect-files .
+call runcl %SRC% --eval "(hs:compile-all %FILES%)"
+rem echo %FILES%
+endlocal
+exit /b
+
+:collect-files
+for /d %%d in (*) do (
+	cd %%d
+	call :collect-files "%~1/%%d"
+	cd ..
+)
+for %%f in (*.hl) do (call :collect-file-1 "%~1/%%f")
+exit /b
+
+:collect-file-1
+set "FILES=\"%~1\" %FILES%"
+exit /b
