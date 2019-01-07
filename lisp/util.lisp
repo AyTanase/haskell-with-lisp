4bd96a6df960b1f30517fa8f5e335289d211d302
AyTanase <ay.tanase@gmail.com>
2019-01-08 07:23:29 +0900

    Define *cl-readtable*

--- lisp/util.lisp
+++ lisp/util.lisp
@@ -1,29 +1,21 @@
 (in-package :hs)
 
 
-(defvar *hs-readtable* (copy-readtable *readtable*))
+(defvar *cl-readtable* *readtable*)
+(defvar *hs-readtable*)
 
-(define-symbol-macro *haskell-case* (readtable-case *hs-readtable*))
+(defmacro read-by (readtable)
+  `#'(lambda (stream &rest args)
+       (declare (ignore args))
+       (let ((*readtable* ,readtable))
+         (read stream t nil t))))
 
-(defmacro def-read-as (name case-name)
-  `(defun ,name (stream &rest args)
-     (declare (ignore args))
-     (let ((default *haskell-case*))
-       (unwind-protect
-            (progn
-              (setf *haskell-case* ,case-name)
-              (read stream t nil t))
-         (setf *haskell-case* default)))))
+(set-dispatch-macro-character #\# #\? (read-by *cl-readtable*))
+(set-dispatch-macro-character #\# #\! (read-by *hs-readtable*))
 
-(def-read-as read-as-lisp :upcase)
-(def-read-as read-as-haskell :preserve)
-
-(let ((*readtable* *hs-readtable*))
-  (set-macro-character #\' (get-macro-character #\') t)
-  (set-dispatch-macro-character #\# #\? #'read-as-lisp)
-  (set-dispatch-macro-character #\# #\! #'read-as-haskell))
-
-(setf *haskell-case* :preserve)
+(setf *hs-readtable* (copy-readtable *cl-readtable*))
+(setf (readtable-case *hs-readtable*) :preserve)
+(set-macro-character #\' (get-macro-character #\') t *hs-readtable*)
 
 
 (defun default-out (src)
