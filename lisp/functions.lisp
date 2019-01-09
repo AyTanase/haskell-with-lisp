4b252e04938d8f8313c5ede6629b76ad240f10c1
AyTanase <ay.tanase@gmail.com>
2019-01-10 08:44:02 +0900

    Make 1+ and 1- work properly

--- lisp/functions.lisp
+++ lisp/functions.lisp
@@ -1,5 +1,30 @@
 (in-package :hs)
 
+(defmacro def-binop-as (name op &key zero one many)
+  (let ((printed (format nil "(~a)" op)))
+    `(progn
+       (defsyntax ,name (&rest args)
+         (cond
+           ((null args) (format t ,(or zero printed)))
+           ((null (cdr args))
+            ,(or one '(haskell (car args))))
+           (t ,(or many `(with-paren (rechask args ,(format nil " ~a " op)))))))
+       (defhasq ,name ,printed))))
+
+(defmacro defbinop (op &rest args) `(def-binop-as ,op ,op ,@args))
+
+(defbinop -> :zero "()")
+(defbinop + :zero "0")
+(defbinop - :one (with-paren (format t "negate ") (haskell (car args))))
+(defbinop * :zero "1")
+(defbinop / :one (with-paren (format t "recip ") (haskell (car args))))
+
+(def-binop-as |and| && :zero "True")
+(def-binop-as |or| "||" :zero "False")
+(def-binop-as |append| ++ :zero "[]")
+(def-binop-as |compose| |.| :zero "id")
+
+
 (defsyntax function (x)
   (if (atom x)
     (haskell x)
@@ -34,31 +59,6 @@
   (format t "[")
   (rechask args ", ")
   (format t "]"))
-
-
-(defmacro def-binop-as (name op &key zero one many)
-  (let ((printed (format nil "(~a)" op)))
-    `(progn
-       (defsyntax ,name (&rest args)
-         (cond
-           ((null args) (format t ,(or zero printed)))
-           ((null (cdr args))
-            ,(or one '(haskell (car args))))
-           (t ,(or many `(with-paren (rechask args ,(format nil " ~a " op)))))))
-       (defhasq ,name ,printed))))
-
-(defmacro defbinop (op &rest args) `(def-binop-as ,op ,op ,@args))
-
-(defbinop -> :zero "()")
-(defbinop + :zero "0")
-(defbinop - :one (with-paren (format t "negate ") (haskell (car args))))
-(defbinop * :zero "1")
-(defbinop / :one (with-paren (format t "recip ") (haskell (car args))))
-
-(def-binop-as |and| && :zero "True")
-(def-binop-as |or| "||" :zero "False")
-(def-binop-as |append| ++ :zero "[]")
-(def-binop-as |compose| |.| :zero "id")
 
 ;; Local Variables:
 ;; eval: (add-cl-indent-rule (quote with-paren) (quote (&body)))
