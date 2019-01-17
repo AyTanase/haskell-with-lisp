089854eb4b6beb6f97f22d6c2f2404b541044314
AyTanase <ay.tanase@gmail.com>
2019-01-17 23:01:51 +0900

    Update sample/

--- sample/Regex/Exec.hs
+++ sample/Regex/Exec.hs
@@ -6,11 +6,11 @@ matchHead = (matchHead' 0) where
   matchHead' n Finite _ = (Just n)
   matchHead' _ (Compare _ _) [] = Nothing
   matchHead' n (Compare x rx) (y:ys)
-    | ((==) x y) = (matchHead' (n + 1) rx ys)
+    | (x == y) = (matchHead' (n + 1) rx ys)
     | otherwise = Nothing
-  matchHead' n (Split p q) xs = ((<|>) (matchHead' n p xs) (matchHead' n q xs))
+  matchHead' n (Split p q) xs = ((matchHead' n p xs) <|> (matchHead' n q xs))
 match :: Num a => (NFA -> [Char] -> (Maybe (a, a)))
 match = (match' 0) where
   match'' n rx xs = (fmap ((,) n) (matchHead rx xs))
   match' n rx [] = (match'' n rx [])
-  match' n rx xs = ((<|>) (match'' n rx xs) (match' (n + 1) rx (tail xs)))
+  match' n rx xs = ((match'' n rx xs) <|> (match' (n + 1) rx (tail xs)))
