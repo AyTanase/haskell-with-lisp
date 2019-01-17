cdd6c9e7ab7a5a9acbbca79c8db9f7334842a86a
AyTanase <ay.tanase@gmail.com>
2019-01-17 23:06:13 +0900

    Update sample/Regex/Exec

--- sample/Regex/Exec.hs
+++ sample/Regex/Exec.hs
@@ -4,10 +4,9 @@ import Control.Applicative
 matchHead :: Num a => (NFA -> [Char] -> (Maybe a))
 matchHead = (matchHead' 0) where
   matchHead' n Finite _ = (Just n)
-  matchHead' _ (Compare _ _) [] = Nothing
   matchHead' n (Compare x rx) (y:ys)
     | (x == y) = (matchHead' (n + 1) rx ys)
-    | otherwise = Nothing
+  matchHead' _ (Compare _ _) _ = Nothing
   matchHead' n (Split p q) xs = ((matchHead' n p xs) <|> (matchHead' n q xs))
 match :: Num a => (NFA -> [Char] -> (Maybe (a, a)))
 match = (match' 0) where
