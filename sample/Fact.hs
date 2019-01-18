cd467b026535dcc54c033ae00eb8d9de4e3dae3b
AyTanase <ay.tanase@gmail.com>
2019-01-18 09:45:00 +0900

    Create sample/Fact

---
+++ sample/Fact.hs
@@ -0,0 +1,8 @@
+module Fact where
+fact0 0 = 1
+fact0 n = (n * (fact0 (n - 1)))
+fact1 = (fact' 1) where
+  fact' p 0 = p
+  fact' p n = (fact' (($!) (*) p n) (n - 1))
+fact2 n = (product [1..n])
+fact3 n = (product (take n [1..]))
