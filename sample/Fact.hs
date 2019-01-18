18c852222b7e58443c89dc94a231529ccfa2fe98
AyTanase <ay.tanase@gmail.com>
2019-01-18 12:15:36 +0900

    Update sample/Fact

--- sample/Fact.hs
+++ sample/Fact.hs
@@ -3,6 +3,6 @@ fact0 0 = 1
 fact0 n = (n * (fact0 (n - 1)))
 fact1 = (fact' 1) where
   fact' p 0 = p
-  fact' p n = (fact' (($!) (*) p n) (n - 1))
+  fact' p n = (fact' (p * n) (n - 1))
 fact2 n = (product [1..n])
 fact3 n = (product (take n [1..]))
