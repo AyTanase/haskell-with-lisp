881897229ab2ae209ee06f6364d1b34e921f6326
AyTanase <ay.tanase@gmail.com>
2019-02-02 12:31:14 +0900

    Update el/haskell-lisp.el

--- el/haskell-lisp.el
+++ el/haskell-lisp.el
@@ -17,17 +17,18 @@
 (cl-indent-rules '(4 &lambda &body) 'class 'uclass 'instance 'uinstance)
 (add-cl-indent-rule 'ulet 'lisp-indent-let-method)
 (add-cl-indent-rule 'ulabels '((&whole 4 &rest (&whole 1 &lambda &body)) &body))
+(add-cl-indent-rule 'do '(&body))
 
 (font-lock-add-keywords
  'haskell-lisp-mode
- '(("(\\(d\\(?:ef\\(?:ine\\|module\\|data\\)\\|ata\\)\\|u\\(?:def\\(?:data\\)?\\|l\\(?:et\\|abels\\)\\|c\\(?:lass\\|ase\\)\\|instance\\|where\\)\\|type\\|enum-from\\|class\\|i\\(?:nstance\\|mport\\)\\|module\\|where\\)\\_>"
+ '(("(\\(d\\(?:ef\\(?:ine\\|module\\|data\\)\\|ata\\)\\|u\\(?:def\\(?:data\\)?\\|l\\(?:et\\|abels\\)\\|c\\(?:lass\\|ase\\)\\|instance\\|where\\)\\|type\\|enum-from\\|class\\|i\\(?:nstance\\|mport\\)\\|module\\|where\\|setf\\)\\_>"
     1 font-lock-keyword-face)
    ("(\\(?:define\\s-+(?\\|type\\s-\\)\\s-*\\([[:lower:]_][[:word:]']*\\)" 1 font-lock-function-name-face)
    ("(type\\s-+(\\([^)]*\\))" 1 font-lock-function-name-face)
    ("(\\(extension\\s-[^)]*\\))" 1 font-lock-preprocessor-face)
    ("(\\(a\\(?:ppe\\)?nd\\|or\\|compose\\|->?\\|[+*/]\\)\\_>" 1 font-lock-variable-name-face)
+   ("#\\(?:\\?\\|\\\\.\\)" . font-lock-negation-char-face)
    ("\\_<\\([[:upper:]][[:word:]']*\\|list\\*?\\|tuple\\|nil\\|cons\\|pair\\)\\_>" 1 font-lock-type-face)
-   ("#\\?" . font-lock-negation-char-face)
    ("\\_<\\?.*?\\_>" . font-lock-builtin-face)))
 
 (define-key haskell-lisp-mode-map (kbd "C-c C-c") 'to-haskell)
