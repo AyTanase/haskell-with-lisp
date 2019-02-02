cb367962fe7c4046f692f354290d7ae8649c9f65
AyTanase <ay.tanase@gmail.com>
2019-02-02 10:53:17 +0900

    Update sample/

--- sample/Regex/Exec.hs
+++ sample/Regex/Exec.hs
@@ -3,13 +3,15 @@ import Common
 import Control.Applicative
 
 matchHead :: Num a => (NFA -> [Char] -> (Maybe a))
-matchHead = (matchHead' 0) where
-  matchHead' n Finite _ = (Just n)
-  matchHead' n (Compare x rx) (_v1:xs)
-    | (x == _v1) = (matchHead' (n + 1) rx xs)
-  matchHead' _ (Compare _ _) _ = Nothing
-  matchHead' n (Split p q) xs = ((matchHead' n p xs) <|> (matchHead' n q xs))
+matchHead = (matchHead' 0)
+  where
+    matchHead' n Finite _ = (Just n)
+    matchHead' n (Compare x rx) (_v1:xs)
+      | (x == _v1) = (matchHead' (n + 1) rx xs)
+    matchHead' _ (Compare _ _) _ = Nothing
+    matchHead' n (Split p q) xs = ((matchHead' n p xs) <|> (matchHead' n q xs))
 
 match :: Num a => (NFA -> [Char] -> (Maybe (a, a)))
-match = (match' 0) where
-  match' n rx xs = ((fmap ((,) n) (matchHead rx xs)) <|> ((=<<) (match' (n + 1) rx) (safeTail xs)))
+match = (match' 0)
+  where
+    match' n rx xs = ((fmap ((,) n) (matchHead rx xs)) <|> ((=<<) (match' (n + 1) rx) (safeTail xs)))
