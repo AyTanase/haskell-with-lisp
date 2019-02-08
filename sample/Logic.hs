8c1361cc50b665238ea733eef6773bde0aeb75f7
AyTanase <ay.tanase@gmail.com>
2019-02-08 19:37:08 +0900

    Update sample/Logic

--- sample/Logic.hs
+++ sample/Logic.hs
@@ -14,13 +14,16 @@ liftCompose = (.) (.)
 
 compose2 :: (a -> b) -> (_v1 -> _v2 -> a) -> _v1 -> _v2 -> b
 compose2 = liftCompose (.)
+
 liftApply :: (b -> c -> d) -> (a -> b) -> (a -> c) -> a -> d
 liftApply = compose2 (<*>) (.)
 
 apply2 :: (_v3 -> _v4 -> a -> b) -> (_v3 -> _v4 -> a) -> _v3 -> _v4 -> b
 apply2 = liftApply (<*>)
+
 apply3 :: (_v5 -> _v6 -> _v7 -> a -> b) -> (_v5 -> _v6 -> _v7 -> a) -> _v5 -> _v6 -> _v7 -> b
 apply3 = liftApply apply2
+
 flip :: (a -> b -> c) -> b -> a -> c
 flip = apply3 const (const const)
 
