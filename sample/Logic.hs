fd3321a2208ac5edb42d22f2ca6f27e67eb0bc7b
AyTanase <ay.tanase@gmail.com>
2019-02-09 09:17:37 +0900

    Update sample/Logic

--- sample/Logic.hs
+++ sample/Logic.hs
@@ -1,20 +1,27 @@
 module Logic where
 import Prelude (Applicative (pure, (<*>)))
 
+const :: a -> b -> a
+const = pure
+
+
 id :: a -> a
-id = pure <*> (pure :: a -> b -> a)
+id = const <*> const
 
 
 liftA :: Applicative f => (a -> b) -> f a -> f b
-liftA = (pure (<*>)) <*> pure
+liftA = (const (<*>)) <*> pure
+
+(.) :: (b -> c) -> (a -> b) -> a -> c
+(.) = liftA
 
 
 liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
-liftA2 = liftA (liftA (<*>)) liftA
+liftA2 = ((.) (<*>)) . liftA
 
 
 flip :: Applicative f => f (a -> b) -> a -> f b
-flip = liftA2 liftA (<*>) (pure pure)
+flip = liftA2 (.) (<*>) (const pure)
 
 join :: (a -> a -> b) -> a -> b
 join = flip (<*>) id
