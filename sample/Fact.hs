dd5643748f59d701cf9ec19a000c296529a654ce
AyTanase <ay.tanase@gmail.com>
2019-02-12 10:28:49 +0900

    Update sample/Fact

--- sample/Fact.hs
+++ sample/Fact.hs
@@ -1,5 +1,7 @@
 module Fact where
 
+fact0, fact1 :: (Num a, Eq a) => a -> a
+
 fact0 0 = 1
 fact0 n = n * fact0 (n - 1)
 
@@ -8,6 +10,9 @@ fact1 = fact' 1
     fact' p 0 = p
     fact' p n = fact' (p * n) (n - 1)
 
+
+fact2 :: (Num a, Enum a) => a -> a
 fact2 n = product [1..n]
 
+fact3 :: (Num a, Enum a) => Int -> a
 fact3 n = product $ take n [1..]
