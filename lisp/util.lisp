e21b8eabdfd7524c427a83d0bfb95650e6ebd7ab
AyTanase <ay.tanase@gmail.com>
2019-02-15 19:19:15 +0900

    Define DEFUN/I, DEFMETHOD/I

--- lisp/util.lisp
+++ lisp/util.lisp
@@ -47,3 +47,40 @@
 
 (defmacro mv-bind (&body body)
   `(multiple-value-bind ,@body))
+
+
+(defun subst-wild-cards (args)
+  (let ((vars nil))
+    (labels ((subst-atom (x)
+               (if (eq x '_)
+                 (let ((var (gensym)))
+                   (push var vars)
+                   var)
+                 x))
+             (subst-cons (x)
+               (if (eq (car x) 'quote)
+                 x
+                 (mapcar #'subst-1 x)))
+             (subst-1 (x)
+               (if (atom x)
+                 (subst-atom x)
+                 (subst-cons x))))
+      (declare (inline subst-atom subst-cons))
+      (let ((new-args (subst-1 args)))
+        (values new-args vars)))))
+
+(defun %def*/i (macro name args &rest body)
+  (mv-bind (new-args vars) (subst-wild-cards args)
+    `(,macro ,name ,new-args
+             (declare (ignore ,@vars))
+             ,@body)))
+
+(defmacro defun/i (&rest args)
+  (apply #'%def*/i 'defun args))
+
+(defmacro defmethod/i (&rest args)
+  (apply #'%def*/i 'defmethod args))
+
+;; Local Variables:
+;; eval: (add-cl-indent-rule (quote mv-bind) (quote (&lambda 4 &body)))
+;; End:
