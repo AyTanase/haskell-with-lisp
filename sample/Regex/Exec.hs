edac148e7c406544d5d49c547752e7bf31bd8788
AyTanase <ay.tanase@gmail.com>
2019-02-19 12:52:56 +0900

    Update sample/Regex/Exec

--- sample/Regex/Exec.hs
+++ sample/Regex/Exec.hs
@@ -7,7 +7,7 @@ exec s = fmap fst . exec' s 0
   where
     exec' Finite n xs = Just (n, xs)
     exec' (Compare x p) n (v1:xs)
-      | (x == v1) = exec' p (n + 1) xs
+      | x == v1 = exec' p (n + 1) xs
     exec' (If p q r) n xs = maybe (exec' r n xs) (uncurry $ exec' q) (exec' p n xs)
     exec' _ _ _ = Nothing
 
