f2162087d4b5b41a14c22facab988ee4def9501d
AyTanase <ay.tanase@gmail.com>
2019-02-27 20:10:10 +0900

    Update bat/cl2hs.bat

--- bat/cl2hs.bat
+++ bat/cl2hs.bat
@@ -7,19 +7,11 @@ exit /b
 
 :compile-all
 set FILES=
-call :collect-files .
+for /r %%F in (*.hl) do (call :collect %%F)
 call runcl %SRC% --eval "(hs:compile-all %FILES%)"
 exit /b
 
-:collect-files
-for %%f in (*.hl) do (call :collect-file-1 "%~1/%%f")
-for /d %%d in (*) do (
-	cd %%d
-	call :collect-files "%~1/%%d"
-	cd ..
-)
-exit /b
-
-:collect-file-1
-set FILES=%FILES% \"%~1\"
+:collect
+set NAME=%~1
+set FILES=%FILES% \"%NAME:\=/%\"
 exit /b
